1a4a1cc2461839e3a64a86f986695786e5ddf863
bpulling21 <brookepulling@gmail.com>
2017-09-25 08:45:57 -0400

    Setup View Controller and Set Up quiz Storyboard

---
+++ SPAPP/QuizViewController.swift
@@ -0,0 +1,35 @@
+//
+//  QuizViewController.swift
+//  SPAPP
+//
+//  Created by Brooke Li Shan on 25/9/2017.
+//  Copyright © 2017 Brooke Pulling. All rights reserved.
+//
+
+import UIKit
+
+class QuizViewController: UIViewController {
+
+    @IBOutlet weak var answerTableView: UITableView!
+    //show the answers
+    @IBOutlet weak var questionLabel: UILabel!
+    //show the question
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
