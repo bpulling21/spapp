72a74aabde38c3cd87fdb755cd319e84e8c6a7ea
bpulling21 <brookepulling@gmail.com>
2017-10-16 08:28:03 -0400

    tableViewImplementation

--- SPAPP/QuizViewController.swift
+++ SPAPP/QuizViewController.swift
@@ -8,18 +8,56 @@
 
 import UIKit
 
-class QuizViewController: UIViewController {
+class QuizViewController: UIViewController, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var answerTableView: UITableView!
     //show the answers
     @IBOutlet weak var questionLabel: UILabel!
     //show the question
+    
+    var questionArray = ["asdf", "ghjk", "lzxc","hi there", "school", "code"]
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        //tableView.delegate = self
+        tableView.dataSource = self
     }
+        // MARK: - Table view data source
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return questionArray.count
+    }
+    
+    
+     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "questionCell", for: indexPath)
+        
+        cell.textLabel?.text = questionArray[indexPath.row]
+    
+        
+        
+        
+        
+        
+        
+        
+        return cell
+    }
+        
+        
+        // Do any additional setup after loading the view.
+    
 
+    // add table view methods
     
 
     /*
