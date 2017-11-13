3dee1f29aeec8bc719bbae0caf51d379437ab7d4
bpulling21 <brookepulling@gmail.com>
2017-11-13 17:48:40 -0500

    added questions

--- SPAPP/Questions.swift
+++ SPAPP/Questions.swift
@@ -44,7 +44,36 @@ import UIKit
         choice2: "22 East 91st Street",
         choice3: "20 East 91st Street",
         correctChoice: "22 East 91st Street")
+
+let question5 = spappQuestion(
+    question: "Where is the Clara Spence tree?",
+    choice1: "The Osbourne Wing",
+    choice2: "Central Park",
+    choice3: "The Townhouse garden",
+    correctChoice: "Central Park")
+
+let question6 = spappQuestion(
+    question: "What sudject does Ms. Krauthamer teach?",
+    choice1: "English",
+    choice2: "French",
+    choice3: "Health",
+    correctChoice: "French")
+
+let question7 = spappQuestion(
+    question: "Which grade is allowed in the elevatorn during the school day?",
+    choice1: "Freshman and 8th graders",
+    choice2: "Seniors only",
+    choice3: "Anyone",
+    correctChoice: "Seniors only")
+
+let question8 = spappQuestion(
+    question: "Challenge: Which spence alum/teacher has won an oscar",
+    choice1: "Gwyneth Paltrow",
+    choice2: "Kerry Washington",
+    choice3: "Mr.Harris",
+    correctChoice: "Gwyneth Paltrow")
+
     
-    var allQuestions: [spappQuestion] = [question1, question2, question3,question4]
+    var allQuestions: [spappQuestion] = [question1, question2, question3,question4, question5, question6, question7, question8]
 //}
 
