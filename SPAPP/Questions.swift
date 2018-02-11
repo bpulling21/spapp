41d831b883dc3b7a53889a5db1128245145b4ff8
Skyler <skylerpulling11@gmail.com>
2018-02-11 09:20:35 -0500

    Cleaned up questions

--- SPAPP/Questions.swift
+++ SPAPP/Questions.swift
@@ -20,9 +20,9 @@ struct spappQuestion {
 }
 
 let question2 = spappQuestion(
-    question: "Which of these rules that apply to this day did NOT come directly from Ms. Clara Spence?",
+    question: "Which of these rules did NOT come from Ms. Clara Spence?",
     choice1:"Serving dessert just once a week",
-    choice2:"Taking the stairs despite the availability of an elevator",
+    choice2:"Students not taking the elevator",
     choice3:"Wearing a uniform",
     choice4:"Required physical education",
     correctChoice: "Wearing a uniform")
@@ -36,7 +36,7 @@ let question3 = spappQuestion(
     correctChoice: "Hillary Clinton")
 
 let question4 = spappQuestion(
-    question: "At what grade are students allowed to leave school for lunch?",
+    question: "At what grade are students allowed to leave for lunch?",
     choice1:"9th",
     choice2:"10th",
     choice3:"11th",
@@ -44,7 +44,7 @@ let question4 = spappQuestion(
     correctChoice: "9th")
 
 let question5 = spappQuestion(
-    question: "In 1932, in response to dire financial difficulties, what drastic action did  the Spence School take to stay open?",
+    question: "In 1932, what did the Spence School do to stay open?",
     choice1:"Merge with the Chandor School",
     choice2:"Accepted boys to increase revenues",
     choice3:"Removed the boarding facilities to curtail expenses",
@@ -61,7 +61,7 @@ let question6 = spappQuestion(
     correctChoice: "Bebbe")
 
 let question7 = spappQuestion(
-    question: "From which university in Massachusetts did Clara Spence graduate?",
+    question: "Where did Clara Spence graduate from college?",
     choice1:"Wellesley College",
     choice2:"Boston University",
     choice3:"Harvard University",
@@ -85,7 +85,7 @@ let question9 = spappQuestion(
     correctChoice: "Spence the Saber")
 
 let question10 = spappQuestion(
-    question: "Which of these has been at one time a Spence Team mascot?",
+    question: "Which of these has been, one time, a Spence Team mascot?",
     choice1:"a penguin",
     choice2:"a monkey",
     choice3:"grandpop",
@@ -101,7 +101,7 @@ let question11 = spappQuestion(
     correctChoice: "120")
 
 let question12 = spappQuestion(
-    question: "How many students did Ms Clara B Spence have when she first opened her school?",
+    question: "How many girls did Ms. Spence originally have?",
     choice1:"5",
     choice2:"10",
     choice3:"15",
@@ -109,7 +109,7 @@ let question12 = spappQuestion(
     correctChoice: "10")
 
 let question13 = spappQuestion(
-    question: "What did Clara B Spence do for a living before starting her own school?",
+    question: "What did Clara Spence do before starting her own school?",
     choice1:"She was an actress",
     choice2:"She earned $5.00 an hour as a tutor",
     choice3:"She was a school teacher",
@@ -129,19 +129,19 @@ let question15 = spappQuestion(
     choice1:"Spence used to be a coed school",
     choice2:"Boys attended Spence school if they were younger siblings",
     choice3:"Boys attended Spence School only in the late 1800’s",
-    choice4:"Boys continued to attend Spence School after moving to its current address on 91st Street.",
+    choice4:"Boys continued to attend Spence School until 1929.",
     correctChoice: "Boys attended Spence School only in the late 1800’s")
 
 let question16 = spappQuestion(
     question:"Which of these awards were given to students?",
-    choice1:"Spoon medal for excellent table manners",
-    choice2:"Posture Captain award for good posture",
+    choice1:"Spoon medal for table manners",
+    choice2:"Posture Captain for good posture",
     choice3:"Honor pin for punctuality and superior character",
     choice4:"All of the above",
     correctChoice: "All of the above")
 
 let question17 = spappQuestion(
-    question: "The Cooper Hewitt Museum used to be the Carnegie Mansion. Upon the passing of Mrs Andrew Carnegie, her estate donated to Spence  a 100 ft by 42ft plot of land that used to be their:",
+    question: "The 91st street land used to be Ms. Carnegie's:",
     choice1:"back lawn",
     choice2:"tennis court",
     choice3:"garden",
@@ -223,11 +223,11 @@ let question26 = spappQuestion(
 
 let question27 = spappQuestion(
     question: "What is Spence's complete address?",
-    choice1: "21 East 91st Street, New York, NY 10028",
-    choice2: "22 East 91st Street, New York, NY 10128",
-    choice3: "20 East 91st Street, New York, NY 10028",
-    choice4: "22 East 91st Street,, New York, NY 1002",
-    correctChoice: "22 East 91st Street, New York, NY 10128")
+    choice1: "21 East 91st Street",
+    choice2: "22 East 91st Street",
+    choice3: "20 East 91st Street",
+    choice4: "22 East 91st Street",
+    correctChoice: "22 East 91st Street")
 
 let question28 = spappQuestion(
     question: "Where is the Clara Spence tree?",
@@ -246,7 +246,7 @@ let question29 = spappQuestion(
     correctChoice: "Non scholae sed vitae discimus")
 
 let question30 = spappQuestion(
-    question: "Which group of students have unrestricted use of the elevator during the school day?",
+    question: "Which students can use the elevator during the day?",
     choice1: "New students",
     choice2: "Seniors only",
     choice3: "Seniors and juniors",
@@ -254,7 +254,7 @@ let question30 = spappQuestion(
     correctChoice: "Seniors only")
 
 let question31 = spappQuestion(
-    question: "Which of these were required of Spence girls in the early years?",
+    question: "Which was required of Spence girls in the early years?",
     choice1: "Clean fingernails",
     choice2: "Straight stocking seams",
     choice3: "Hats and gloves when stepping out the door",
@@ -270,7 +270,7 @@ let question32 = spappQuestion(
     correctChoice: "80%")
 
 let question33 = spappQuestion(
-    question: "How many times do you have to be late to school to receive a detention?",
+    question: "You receive a detention after being late _____ times?",
     choice1: "3",
     choice2: "5",
     choice3: "7",
@@ -278,7 +278,7 @@ let question33 = spappQuestion(
     correctChoice: "5")
 
 let question34 = spappQuestion(
-    question: "What is the largest number of free periods a senior currently has?",
+    question: "What is the largest number of free periods someone has?",
     choice1: "7",
     choice2: "8",
     choice3: "9",
@@ -294,7 +294,7 @@ let question35 = spappQuestion(
     correctChoice: "1003")
 
 let question36 = spappQuestion(
-    question: "The _________ consolate lies across the street from Spence",
+    question: "Which consolate is across the street from Spence?",
     choice1:"Romanian",
     choice2:"Chinese",
     choice3:"Russian",
@@ -302,7 +302,7 @@ let question36 = spappQuestion(
     correctChoice: "Russian")
 
 let question37 = spappQuestion(
-    question: "How many students take Greek(independent study) at Spence?",
+    question: "How many students take Greek at Spence?",
     choice1:"12",
     choice2:"16",
     choice3:"1",
@@ -342,15 +342,6 @@ let question41 = spappQuestion(
     choice4:"12th",
     correctChoice: "5th")
 
-
-let question42 = spappQuestion(
-    question: "Where is the door that is on the school logo?",
-    choice1:"The door of the original Spence School townhouse on 6 West 48th Street",
-    choice2:"The door of the Spence School townhouse on 30 West 55th Street",
-    choice3:"The door of the Spence School townhouse on 56 East 93rd Street",
-    choice4:"The door of the main Spence School townhouse on 22 East 91st street",
-    correctChoice: "The door of the main Spence School townhouse on 22 East 91st street")
-
 let question43 = spappQuestion(
     question: "Which grade has the most students?",
     choice1: "Kindergarten",
@@ -368,18 +359,18 @@ let question44 = spappQuestion(
     correctChoice: "clara spence")
 
 let question45 = spappQuestion(
-    question: "Where can you find the door that is on the school logo? ",
-    choice1: "Door of the original Spence School Townhouse on 6 West 48th Street",
-    choice2: "Door of the old Spence School townhouse on 30 West 55th Street ",
-    choice3: "Door of the current Spence building on 22 East 91st street",
-    choice4: "Door of the old lower school building on 56 East 93rd Street" ,
-    correctChoice: "Door of the current Spence building on 22 East 91st street")
+    question: "Where is the door on the school logo? ",
+    choice1: "Door of the original Spence School Townhouse",
+    choice2: "Door of the second oldest Spence School townhouse ",
+    choice3: "Door of the current Spence building",
+    choice4: "Door of the lower school building" ,
+    correctChoice: "Door of the current Spence building")
 
 
 
 var levelOneQuestions: [spappQuestion] = [question24, question18, question27, question11, question38, question8, question39, question19, question36, question29, question14, question32, question43, question4]
 
-var levelTwoQuestions: [spappQuestion] = [question35, question41, question3,question42, question37, question21, question25, question26, question28, question33, question34, question9, question10, question30]
+var levelTwoQuestions: [spappQuestion] = [question35, question41, question3, question37, question21, question25, question26, question28, question33, question34, question9, question10, question30]
 
 var levelThreeQuestions: [spappQuestion] = [question6, question7, question12, question13, question40, question22, question23, question15, question17, question16, question31, question5, question20, question2, question44, question45]
 
